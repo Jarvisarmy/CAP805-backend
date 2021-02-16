942c6fa0235623836fefd418f8e4664f705e3088
zeduo zhang <1102297439zzd@gmail.com>
2021-02-16 01:13:01 -0500

    update

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ var bodyParser = require("body-parser");
 
 var dataModule = require("./modules/serverDataModule.js");
 
-
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(function(req, res, next) {
     // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
