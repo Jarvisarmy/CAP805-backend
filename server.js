1328f248f649c3a741b1ae2811b914069104b203
zeduo zhang <1102297439zzd@gmail.com>
2021-02-16 02:09:43 -0500

    update

--- server.js
+++ server.js
@@ -5,25 +5,25 @@ var path = require("path");
 var bodyParser = require("body-parser");
 
 var dataModule = require("./modules/serverDataModule.js");
-/*
+
 app.use(function(req, res, next) {
     // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  req.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  req.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   
   // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  req.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  req.setHeader('Access-Control-Allow-Credentials', true);
 
   // Pass to next layer of middleware
   next();
 });
-*/
+
 app.get("/", (req,res)=>{
     res.redirect("/games");
 })
