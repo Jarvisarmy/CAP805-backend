8856ccb1a64810a2c5f6f42c08a018e41a9a3125
zeduo zhang <1102297439zzd@gmail.com>
2021-02-16 00:49:39 -0500

    update

--- server.js
+++ server.js
@@ -22,9 +22,7 @@ app.use(function(req, res, next) {
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
   
-  bodyParser.urlencoded({extended: true})
   // Pass to next layer of middleware
-
   next();
   });
 
