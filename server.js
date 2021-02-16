9346a411f8d628ba3b886167ce0c41a7896a316c
zeduo zhang <1102297439zzd@gmail.com>
2021-02-15 20:59:50 -0500

    update

--- server.js
+++ server.js
@@ -2,12 +2,27 @@ var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
 var path = require("path");
-
+var cors = require("cors");
 var bodyParser = require("body-parser");
 
 var dataModule = require("./modules/serverDataModule.js");
 
+const allowedOrigins = ["http://localhost:3000","http://localhost:8080"];
 
+app.use(
+    cors({
+        origin: function(origin, callback) {
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.indexOf(origin) === -1) {
+                var msg =
+                    "The CORS policy for this site does not " +
+                    "allow access from the specified Origin.";
+                return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+        }
+    })
+);
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", (req,res)=>{
